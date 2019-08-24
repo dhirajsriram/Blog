f47f61f990e6c98d54d228dbfd35dbd730912ee9
Dhriaj Sriram <dhirajsriram3194@gmail.com>
2019-08-24 01:17:46 -0400

    changes

--- src/common/blog/blog.tsx
+++ src/common/blog/blog.tsx
@@ -6,27 +6,36 @@ import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
+import Moment from "react-moment";
 
 const useStyles = makeStyles({
   card: {
-    marginBottom: "24px",
+    marginBottom: "24px"
   },
-  author:{
-    color:"gray",
-    fontSize:15
+  author: {
+    textAlign:"right",
+    color: "gray",
+    fontSize: 15,
+    paddingRight:10,
+    fontStyle:"italic",
+    marginLeft:"auto",
   },
-  content:{
-      marginTop:20,
-      maxHeight:150,
-      whiteSpace:"nowrap",
-      overflow:"hidden",
-      textOverflow:"ellipsis"
+  date: {
+    fontSize: 15
+  },
+  content: {
+    marginTop: 15,
+    maxHeight: 150,
+    whiteSpace: "nowrap",
+    overflow: "hidden",
+    textOverflow: "ellipsis"
   }
 });
 
 export default function Blog(props: any) {
   const classes = useStyles();
-  const blog = props.item
+  const blog = props.item;
+  const dateToFormat = blog.published;
   return (
     <Card className={classes.card}>
       <CardActionArea>
@@ -34,21 +43,27 @@ export default function Blog(props: any) {
           <Typography gutterBottom variant="h5" component="h2">
             {blog.title}
           </Typography>
-          <Typography gutterBottom className={classes.author} variant="subtitle1" component="h3">
-            {blog.author.displayName}
+          <Typography gutterBottom className={classes.date} variant="subtitle1" component="h3">
+            <Moment format="LL" date={dateToFormat} />
           </Typography>
+          <Typography gutterBottom className={classes.date} variant="subtitle1" component="h3" />
           <Typography className={classes.content} variant="body2" color="textSecondary" component="div">
-            <div dangerouslySetInnerHTML={{__html:blog.content}}></div>
+            <div dangerouslySetInnerHTML={{ __html: blog.content }} />
           </Typography>
+          
         </CardContent>
       </CardActionArea>
+      
       <CardActions>
-        <Button size="small" color="primary">
-          Share
+        <Button size="small" color="primary" onClick={()=>props.deleteBlog(blog.id)}>
+          Delete
         </Button>
         <Button size="small" color="primary">
           Learn More
         </Button>
+        <Typography gutterBottom className={classes.author} variant="subtitle1" component="h3">
+          - {blog.author.displayName}
+          </Typography>
       </CardActions>
     </Card>
   );
--- src/pages/listing.tsx
+++ src/pages/listing.tsx
@@ -1,32 +1,62 @@
 import React, { useEffect, useState } from "react";
 import { withRouter } from "react-router";
 import Blog from "../common/blog/blog";
+import Pagination from "material-ui-flat-pagination";
 
 const Listing = (props: any) => {
   const [state, setState] = useState();
+  const [offset,setOffset] = useState(0);
+  const [page,setPage] = useState(0);
   const url =
-    "https://content.googleapis.com/blogger/v3/blogs/3213900/posts?fetchBodies=true&fetchImages=true&maxResults=40&orderBy=published&key=AIzaSyCnz169tp7MAkt0ef4AF4Xc_mBNFpU-aas";
+    "https://content.googleapis.com/blogger/v3/blogs/15045980/posts?fetchBodies=true&fetchImages=true&maxResults=500&orderBy=published&key=AIzaSyCnz169tp7MAkt0ef4AF4Xc_mBNFpU-aas";
   useEffect(() => {
     fetch(url)
       .then(response => {
         return response.json();
       })
-      .then(json => {
-        let blogs = json;
-        setState({ data: blogs });
+      .then(response => {
+        let blogs = response.items;
+        blogs.map((blog:any,index:string)=>{
+          console.log(blog.title)
+        })
+        setState({data:blogs})
       })
       .catch(err => {
         console.log("Fetch problem: " + err.message);
       });
   }, []);
 
+  const deleteBlog = (id:string) =>{
+    var index = state.data.indexOf(state.data.find((item:any) => item.id === id))
+    let tempArr = state.data;
+    tempArr.splice(index,1);
+    setState({data:tempArr})
+    console.log(tempArr)
+  }
+
+  const handleClick = (offset:number) => {
+    setOffset(offset);
+  }
+
+
   return (
     <div className="listing-page">
-      {(state && state.data && state.data.items.map((item:object,index:string)=>{
+      {(state && state.data && state.data.map((item:object,index:any)=>{
         return(
-          <Blog item={item} key={index}/>
+          <React.Fragment>{
+            (index < (offset + 20)) && (index > offset) &&
+          <Blog item={item} key={index} deleteBlog={deleteBlog}/>}
+          </React.Fragment>
         )
       }) )}
+      <div className="pagination-controls">
+       <Pagination
+          limit={20}
+          offset={offset}
+          total={state ? state.data.length : 0}
+          onClick={(e, offset) => handleClick(offset)}
+        />
+        </div>
     </div>
   );
 };
