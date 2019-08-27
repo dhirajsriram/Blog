9b4524a54428165ca7dad90dd9111b58a5724678
Dhiraj <admin@admins-MacBook-Pro.local>
2019-08-26 20:21:58 -0400

    Added checkmark for add blog

--- src/pages/addblog.tsx
+++ src/pages/addblog.tsx
@@ -6,42 +6,51 @@ import {
     DatePicker, MuiPickersUtilsProvider,
 } from '@material-ui/pickers';
 import DateFnsUtils from '@date-io/date-fns';
-const useStyles = makeStyles((theme: Theme) =>
-    createStyles({
-        container: {
-            display: 'flex',
-            flexWrap: 'wrap',
-            padding: 24
-        },
-        button: {
-            margin: theme.spacing(1),
-        },
-        date: {
-            width: "100%"
-        },
-        textField: {
-            width: "100%",
-        },
-    }),
-);
+import { withRouter } from 'react-router';
+
+
 
 interface State {
     title: any;
     date: any;
     content: any;
-    author:any
+    author: any;
+    submitted: boolean
 }
 
-export default function Addblog(props:any) {
-    const classes = useStyles();
+const Addblog = (props: any) => {
+
 
     const [values, setValues] = React.useState<State>({
         title: true,
         date: new Date().toJSON().slice(0, 10).replace(/-/g, '/'),
         content: true,
-        author:true
+        author: true,
+        submitted: false
     });
 
+    const useStyles = makeStyles((theme: Theme) =>
+        createStyles({
+            container: {
+                display: 'flex',
+                flexWrap: 'wrap',
+                padding: 24
+            },
+            checkmarkImg:{
+                display:props.submitted?"block":"none"
+            },
+            button: {
+                margin: theme.spacing(1),
+            },
+            date: {
+                width: "100%"
+            },
+            textField: {
+                width: "100%",
+            },
+        }),
+    );
+    const classes = useStyles();
     const handleChange = (name: keyof State) => (event: React.ChangeEvent<HTMLInputElement>) => {
         setValues({ ...values, [name]: event.target.value });
     };
@@ -59,80 +68,90 @@ export default function Addblog(props:any) {
             "published": values.date.toString(),
             "title": values.title,
             "content": values.content,
-            "labels":[values.author],
-            "label":[values.author],
+            "labels": [values.author],
+            "label": [values.author],
             "author": {
-              "displayName": values.author,
+                "displayName": values.author,
             },
-          }
-          props.addBlog(blogObj)
+        }
+        props.addBlog(blogObj)
+        setValues({ ...values, submitted: true })
+        setTimeout(() => {
+            props.history.push("/")
+        }, 1500);
     }
 
 
     return (
         <div className="add-blog-page">
             <Paper>
-                <form className={classes.container} noValidate autoComplete="off">
-                    <Typography variant="h6" component="h6" color="primary">Add Blog</Typography>
-                    <Grid container spacing={2}>
-                        <Grid xs={12} md={12} item><TextField
-                            id="blog-title"
-                            label="Title"
-                            required={true}
-                            error={values.title ? undefined : true}
-                            className={classes.textField}
-                            defaultValue=""
-                            onChange={handleChange('title')}
-                            margin="normal"
-                            variant="outlined"
-                        /></Grid>
-                        <Grid item xs={6} md={6}><MuiPickersUtilsProvider utils={DateFnsUtils}>
-                            <Grid container justify="space-around">
-                                <DatePicker
-                                    margin="normal"
-                                    openTo="year"
-                                    format="dd/MM/yyyy"
-                                    inputVariant="outlined"
-                                    className={classes.date}
-                                    label="Date of birth"
-                                    views={["year", "month", "date"]}
-                                    value={values.date}
-                                    onChange={handleDateChange}
-                                /></Grid>
-                        </MuiPickersUtilsProvider></Grid>
-                        <Grid item xs={6} md={6}>
-                            <TextField
-                            id="blog-author"
-                            label="Author"
-                            error={values.author ? undefined : true}
-                            className={classes.textField}
-                            defaultValue=""
-                            onChange={handleChange('author')}
-                            margin="normal"
-                            variant="outlined"
-                            required={true}
-                        /></Grid>
-                        <Grid item xs={12} md={12}>
-                            <TextField
-                            required
-                                id="blog-content"
-                                label="Content"
-                                error={values.content ? undefined : true}
-                                multiline
-                                rows="10"
-                                defaultValue=""
-                                onChange={handleChange('content')}
+                {!values.submitted ?
+                    <form className={classes.container} noValidate autoComplete="off">
+                        <Typography variant="h6" component="h6" color="primary">Add Blog</Typography>
+                        <Grid container spacing={2}>
+                            <Grid xs={12} md={12} item><TextField
+                                id="blog-title"
+                                label="Title"
+                                required={true}
+                                error={values.title ? undefined : true}
                                 className={classes.textField}
+                                defaultValue=""
+                                onChange={handleChange('title')}
                                 margin="normal"
                                 variant="outlined"
-                            />
-                        </Grid>
-                        <Grid xs={12} md={12} item>
-                            <Button disabled={values.author && values.content && values.date && values.title ? undefined : true} onClick={handleSubmit} variant="contained" color="primary" className={classes.button}>
-                                SUBMIT
+                            /></Grid>
+                            <Grid item xs={6} md={6}><MuiPickersUtilsProvider utils={DateFnsUtils}>
+                                <Grid container justify="space-around">
+                                    <DatePicker
+                                        margin="normal"
+                                        openTo="year"
+                                        format="dd/MM/yyyy"
+                                        inputVariant="outlined"
+                                        className={classes.date}
+                                        label="Date of birth"
+                                        views={["year", "month", "date"]}
+                                        value={values.date}
+                                        onChange={handleDateChange}
+                                    /></Grid>
+                            </MuiPickersUtilsProvider></Grid>
+                            <Grid item xs={6} md={6}>
+                                <TextField
+                                    id="blog-author"
+                                    label="Author"
+                                    error={values.author ? undefined : true}
+                                    className={classes.textField}
+                                    defaultValue=""
+                                    onChange={handleChange('author')}
+                                    margin="normal"
+                                    variant="outlined"
+                                    required={true}
+                                /></Grid>
+                            <Grid item xs={12} md={12}>
+                                <TextField
+                                    required
+                                    id="blog-content"
+                                    label="Content"
+                                    error={values.content ? undefined : true}
+                                    multiline
+                                    rows="10"
+                                    defaultValue=""
+                                    onChange={handleChange('content')}
+                                    className={classes.textField}
+                                    margin="normal"
+                                    variant="outlined"
+                                />
+                            </Grid>
+                            <Grid xs={12} md={12} item>
+                                <Button disabled={values.author && values.content && values.date && values.title ? undefined : true} onClick={handleSubmit} variant="contained" color="primary" className={classes.button}>
+                                    SUBMIT
                             </Button>
+                            </Grid>
                         </Grid>
-                    </Grid>
-                </form></Paper></div>
+                    </form> :
+                    <Typography align="center">
+                        <img src={require("../assets/checkmark.gif")} alt="checkmark-gif" />
+                    </Typography>}</Paper></div>
     );
 }
+
+export default withRouter(Addblog) 
